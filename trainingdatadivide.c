0c2dbd6b31071e080ed6ba50bb46051774e18c37
prajakta-pathak <prarocks4u@gmail.com>
2013-05-30 12:53:54 +0530

    added

--- trainingdatadivide.c
+++ trainingdatadivide.c
@@ -4,23 +4,20 @@
 int main()
 {
 	FILE *f0,*f1;
-	f0=fopen("data.txt","r");
-	f1=fopen("trainingdata.txt","w+");
-	int i=0;
+	f0=fopen("trainingdata.txt","r");
 	char c;
-	while(i!=1500)
+	int i;
+	for(i=0;i<2;i++)
 	{
+		f1=fopen("new1.txt","a");		
 		while((c=fgetc(f0))!=' ')
 		{
-		
 			putc(c,f1);
 		}
-		i++;
 		fseek(f0,1,SEEK_CUR);
 		c=fgetc(f0);
-		putc(' ',f1);
 		putc('\n',f1);
+		fclose(f1);
 	}
 	fclose(f0);
-	fclose(f1);
-}
+}	
