529380d61b96c8f38c64d760d7afd5b337a794bb
prajakta-pathak <prarocks4u@gmail.com>
2013-06-05 18:05:13 +0530

    added

--- src/digit.c
+++ src/digit.c
@@ -4,6 +4,621 @@
 #include<math.h>
 #include"digit.h"
 
+void data_set_divide()
+{
+	FILE *f,*f1,*f2,*f3,*f4,*f5,*f6,*f7,*f8,*f9,*f0,*d;
+	f=fopen("data/data.txt","r");
+	f0=fopen("data/zero.txt","a");
+	f1=fopen("data/one.txt","a");
+	f2=fopen("data/two.txt","a");
+	f3=fopen("data/three.txt","a");
+	f4=fopen("data/four.txt","a");
+	f5=fopen("data/five.txt","a");
+	f6=fopen("data/six.txt","a");
+	f7=fopen("data/seven.txt","a");
+	f8=fopen("data/eight.txt","a");
+	f9=fopen("data/nine.txt","a");
+	d=fopen("data/dummy.txt","w+");
+	int count=0,n,i;
+	n=31218/33;
+	char c,str[3],ch;
+	char new='\n';
+	char temp0[]=" 0";
+	char temp1[]=" 1";
+	char temp2[]=" 2";
+	char temp3[]=" 3";
+	char temp4[]=" 4";
+	char temp5[]=" 5";
+	char temp6[]=" 6";
+	char temp7[]=" 7";
+	char temp8[]=" 8";
+	char temp9[]=" 9";
+	for(i=0;i<n;i++)
+	{
+		while(count!=33)
+		{
+			ch=fgetc(f);
+			
+			if(ch!=new)
+			{
+				putc(ch,d);
+			}
+			else
+			{
+				putc(ch,d);
+				count++;
+			}
+		}	
+		count=0;
+		fseek(d,-3,SEEK_END);	
+		fgets(str,sizeof(str),d);
+		if(strcmp(str,temp0)==0)
+		{
+			fprintf(f0,"%d\n",i);
+			fclose(d);
+			d=fopen("data/dummy.txt","w+");
+		}
+		else if(strcmp(str,temp1)==0)
+		{
+			fprintf(f1,"%d\n",i);
+			fclose(d);
+			d=fopen("data/dummy.txt","w+");
+		}		
+		else if(strcmp(str,temp2)==0)
+		{
+			fprintf(f2,"%d\n",i);
+			fclose(d);
+			d=fopen("data/dummy.txt","w+");
+		}	
+		else if(strcmp(str,temp3)==0)
+		{
+			fprintf(f3,"%d\n",i);
+			fclose(d);
+			d=fopen("data/dummy.txt","w+");
+		}	
+		else if(strcmp(str,temp4)==0)
+		{
+			fprintf(f4,"%d\n",i);
+			fclose(d);
+			d=fopen("data/dummy.txt","w+");
+		}	
+		else if(strcmp(str,temp5)==0)
+		{
+			fprintf(f5,"%d\n",i);
+			fclose(d);
+			d=fopen("data/dummy.txt","w+");
+		}	
+		else if(strcmp(str,temp6)==0)
+		{
+			fprintf(f6,"%d\n",i);
+			fclose(d);
+			d=fopen("data/dummy.txt","w+");
+		}	
+		else if(strcmp(str,temp7)==0)
+		{
+			fprintf(f7,"%d\n",i);
+			fclose(d);
+			d=fopen("data/dummy.txt","w+");
+		}	
+		else if(strcmp(str,temp8)==0)
+		{
+			fprintf(f8,"%d\n",i);
+			fclose(d);
+			d=fopen("data/dummy.txt","w+");
+		}	
+		else if(strcmp(str,temp9)==0)
+		{
+			fprintf(f9,"%d\n",i);
+			fclose(d);
+			d=fopen("data/dummy.txt","w+");
+		}	
+	}
+	fclose(f);
+	fclose(d);
+	fclose(f0);
+	fclose(f1);
+	fclose(f2);
+	fclose(f3);
+	fclose(f4);
+	fclose(f5);
+	fclose(f6);
+	fclose(f7);
+	fclose(f8);
+	fclose(f9);
+}
+
+void create_training_data()
+{
+	FILE *f0,*f1;
+	f0=fopen("data/data.txt","r");
+	f1=fopen("data/trainingdata.txt","w+");
+	int i=0;
+	char c;
+	while(i!=1500)
+	{
+		while((c=fgetc(f0))!=' ')
+		{
+		
+			putc(c,f1);
+		}
+		i++;
+		fseek(f0,1,SEEK_CUR);
+		c=fgetc(f0);
+		putc(' ',f1);
+		putc('\n',f1);
+	}
+	fclose(f0);
+	fclose(f1);
+}
+
+void training_data_divide()
+{
+	FILE *f0,*f1;
+	f0=fopen("data/trainingdata.txt","r");
+	char c;
+	char fname[16];
+	int i;
+	for(i=0;i<1500;i++)
+	{
+		sprintf(fname,"data/%d-digit.txt",i);
+		f1=fopen(fname,"a");		
+		while((c=fgetc(f0))!=' ')
+		{
+			putc(c,f1);
+		}
+		c=fgetc(f0);
+		fclose(f1);
+	}
+	fclose(f0);
+}	
+
+void ave_construct()
+{
+	float num[32][32];
+	FILE *f0,*n0,*f1;
+	int k,i,j,dummy;
+	char c,fname[16];
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			num[i][j]=0;
+		}
+	}	
+
+	f0=fopen("data/zero.txt","r");
+	n0=fopen("data/zeroave.txt","a");
+	for(k=0;k<50;k++)
+	{
+		fscanf(f0,"%d",&dummy);
+		sprintf(fname,"data/%d-digit.txt",dummy);
+		f1=fopen(fname,"r");	
+		printf("%s",fname);	
+		for(i=0;i<32;i++)
+		{
+			for(j=0;j<32;j++)
+			{
+				c=fgetc(f1);
+				if(c=='0')
+				num[i][j]=num[i][j]+0;
+				else
+				num[i][j]=num[i][j]+1;
+			}
+	
+		}	
+		fclose(f1);		
+		
+	}
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			fprintf(n0,"%f",num[i][j]/50);
+			fprintf(n0,"%c",' ');
+		}
+		fprintf(n0,"%c",'\n');
+	}	
+	
+	fclose(f0);
+	fclose(n0);
+
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			num[i][j]=0;
+		}
+	}	
+
+	f0=fopen("data/one.txt","r");
+	n0=fopen("data/oneave.txt","a");
+	for(k=0;k<50;k++)
+	{
+		fscanf(f0,"%d",&dummy);
+		sprintf(fname,"data/%d-digit.txt",dummy);
+		f1=fopen(fname,"r");	
+		printf("%s",fname);	
+		for(i=0;i<32;i++)
+		{
+			for(j=0;j<32;j++)
+			{
+				c=fgetc(f1);
+				if(c=='0')
+				num[i][j]=num[i][j]+0;
+				else
+				num[i][j]=num[i][j]+1;
+			}
+	
+		}	
+		fclose(f1);		
+		
+	}
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			fprintf(n0,"%f",num[i][j]/50);
+			fprintf(n0,"%c",' ');
+		}
+		fprintf(n0,"%c",'\n');
+	}	
+	
+	fclose(f0);
+	fclose(n0);
+
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			num[i][j]=0;
+		}
+	}	
+
+	f0=fopen("data/two.txt","r");
+	n0=fopen("data/twoave.txt","a");
+	for(k=0;k<50;k++)
+	{
+		fscanf(f0,"%d",&dummy);
+		sprintf(fname,"data/%d-digit.txt",dummy);
+		f1=fopen(fname,"r");	
+		printf("%s",fname);	
+		for(i=0;i<32;i++)
+		{
+			for(j=0;j<32;j++)
+			{
+				c=fgetc(f1);
+				if(c=='0')
+				num[i][j]=num[i][j]+0;
+				else
+				num[i][j]=num[i][j]+1;
+			}
+	
+		}	
+		fclose(f1);		
+		
+	}
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			fprintf(n0,"%f",num[i][j]/50);
+			fprintf(n0,"%c",' ');
+		}
+		fprintf(n0,"%c",'\n');
+	}	
+	
+	fclose(f0);
+	fclose(n0);
+
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			num[i][j]=0;
+		}
+	}	
+
+	f0=fopen("data/three.txt","r");
+	n0=fopen("data/threeave.txt","a");
+	for(k=0;k<50;k++)
+	{
+		fscanf(f0,"%d",&dummy);
+		sprintf(fname,"data/%d-digit.txt",dummy);
+		f1=fopen(fname,"r");	
+		printf("%s",fname);	
+		for(i=0;i<32;i++)
+		{
+			for(j=0;j<32;j++)
+			{
+				c=fgetc(f1);
+				if(c=='0')
+				num[i][j]=num[i][j]+0;
+				else
+				num[i][j]=num[i][j]+1;
+			}
+	
+		}	
+		fclose(f1);		
+		
+	}
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			fprintf(n0,"%f",num[i][j]/50);
+			fprintf(n0,"%c",' ');
+		}
+		fprintf(n0,"%c",'\n');
+	}	
+	
+	fclose(f0);
+	fclose(n0);
+
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			num[i][j]=0;
+		}
+	}	
+
+	f0=fopen("data/four.txt","r");
+	n0=fopen("data/fourave.txt","a");
+	for(k=0;k<50;k++)
+	{
+		fscanf(f0,"%d",&dummy);
+		sprintf(fname,"data/%d-digit.txt",dummy);
+		f1=fopen(fname,"r");	
+		printf("%s",fname);	
+		for(i=0;i<32;i++)
+		{
+			for(j=0;j<32;j++)
+			{
+				c=fgetc(f1);
+				if(c=='0')
+				num[i][j]=num[i][j]+0;
+				else
+				num[i][j]=num[i][j]+1;
+			}
+	
+		}	
+		fclose(f1);		
+		
+	}
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			fprintf(n0,"%f",num[i][j]/50);
+			fprintf(n0,"%c",' ');
+		}
+		fprintf(n0,"%c",'\n');
+	}	
+	
+	fclose(f0);
+	fclose(n0);
+	
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			num[i][j]=0;
+		}
+	}	
+
+	f0=fopen("data/five.txt","r");
+	n0=fopen("data/fiveave.txt","a");
+	for(k=0;k<50;k++)
+	{
+		fscanf(f0,"%d",&dummy);
+		sprintf(fname,"data/%d-digit.txt",dummy);
+		f1=fopen(fname,"r");	
+		printf("%s",fname);	
+		for(i=0;i<32;i++)
+		{
+			for(j=0;j<32;j++)
+			{
+				c=fgetc(f1);
+				if(c=='0')
+				num[i][j]=num[i][j]+0;
+				else
+				num[i][j]=num[i][j]+1;
+			}
+	
+		}	
+		fclose(f1);		
+		
+	}
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			fprintf(n0,"%f",num[i][j]/50);
+			fprintf(n0,"%c",' ');
+		}
+		fprintf(n0,"%c",'\n');
+	}	
+	
+	fclose(f0);
+	fclose(n0);
+
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			num[i][j]=0;
+		}
+	}	
+
+	f0=fopen("data/six.txt","r");
+	n0=fopen("data/sixave.txt","a");
+	for(k=0;k<50;k++)
+	{
+		fscanf(f0,"%d",&dummy);
+		sprintf(fname,"data/%d-digit.txt",dummy);
+		f1=fopen(fname,"r");	
+		printf("%s",fname);	
+		for(i=0;i<32;i++)
+		{
+			for(j=0;j<32;j++)
+			{
+				c=fgetc(f1);
+				if(c=='0')
+				num[i][j]=num[i][j]+0;
+				else
+				num[i][j]=num[i][j]+1;
+			}
+	
+		}	
+		fclose(f1);		
+		
+	}
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			fprintf(n0,"%f",num[i][j]/50);
+			fprintf(n0,"%c",' ');
+		}
+		fprintf(n0,"%c",'\n');
+	}	
+	
+	fclose(f0);
+	fclose(n0);
+
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			num[i][j]=0;
+		}
+	}	
+
+	f0=fopen("data/seven.txt","r");
+	n0=fopen("data/sevenave.txt","a");
+	for(k=0;k<50;k++)
+	{
+		fscanf(f0,"%d",&dummy);
+		sprintf(fname,"data/%d-digit.txt",dummy);
+		f1=fopen(fname,"r");	
+		printf("%s",fname);	
+		for(i=0;i<32;i++)
+		{
+			for(j=0;j<32;j++)
+			{
+				c=fgetc(f1);
+				if(c=='0')
+				num[i][j]=num[i][j]+0;
+				else
+				num[i][j]=num[i][j]+1;
+			}
+	
+		}	
+		fclose(f1);		
+		
+	}
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			fprintf(n0,"%f",num[i][j]/50);
+			fprintf(n0,"%c",' ');
+		}
+		fprintf(n0,"%c",'\n');
+	}	
+	
+	fclose(f0);
+	fclose(n0);
+
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			num[i][j]=0;
+		}
+	}	
+
+	f0=fopen("data/eight.txt","r");
+	n0=fopen("data/eightave.txt","a");
+	for(k=0;k<50;k++)
+	{
+		fscanf(f0,"%d",&dummy);
+		sprintf(fname,"data/%d-digit.txt",dummy);
+		f1=fopen(fname,"r");	
+		printf("%s",fname);	
+		for(i=0;i<32;i++)
+		{
+			for(j=0;j<32;j++)
+			{
+				c=fgetc(f1);
+				if(c=='0')
+				num[i][j]=num[i][j]+0;
+				else
+				num[i][j]=num[i][j]+1;
+			}
+	
+		}	
+		fclose(f1);		
+		
+	}
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			fprintf(n0,"%f",num[i][j]/50);
+			fprintf(n0,"%c",' ');
+		}
+		fprintf(n0,"%c",'\n');
+	}	
+	
+	fclose(f0);
+	fclose(n0);
+
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			num[i][j]=0;
+		}
+	}	
+
+	f0=fopen("data/nine.txt","r");
+	n0=fopen("data/nineave.txt","a");
+	for(k=0;k<50;k++)
+	{
+		fscanf(f0,"%d",&dummy);
+		sprintf(fname,"data/%d-digit.txt",dummy);
+		f1=fopen(fname,"r");	
+		printf("%s",fname);	
+		for(i=0;i<32;i++)
+		{
+			for(j=0;j<32;j++)
+			{
+				c=fgetc(f1);
+				if(c=='0')
+				num[i][j]=num[i][j]+0;
+				else
+				num[i][j]=num[i][j]+1;
+			}
+	
+		}	
+		fclose(f1);		
+		
+	}
+	for(i=0;i<32;i++)
+	{
+		for(j=0;j<32;j++)
+		{
+			fprintf(n0,"%f",num[i][j]/50);
+			fprintf(n0,"%c",' ');
+		}
+		fprintf(n0,"%c",'\n');
+	}	
+	
+	fclose(f0);
+	fclose(n0);
+}
+
 int prediction(char name[])
 {
 	int n;
--- src/digit.h
+++ src/digit.h
@@ -3,3 +3,7 @@ int digitrec(char input[]);
 float distancesq(char in[],char ave[]);
 int smallest(float num[]);
 int prediction(char name[]);
+void ave_construct();
+void create_training_data();
+void data_set_divide();
+void training_data_divide();
